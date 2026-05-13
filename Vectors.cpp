0326d615957395d87375811b854a82b365aa7bc3
kapil soni <168884860+userdefined111@users.noreply.github.com>
2026-05-13 12:43:23 +0530

    Add files via upload

---
+++ Vectors.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int size;
+    cin>> size;
+    int *arr = new int [size];
+    int x=1
+    return 0;
+}
+ 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+class print{
+// public:
+//     void show(int a){
+//         cout << "Integer: " << a << endl;
+//     }
+//     void show(string s){
+//         cout << "String: " << s << endl;
+//     }
+// };
