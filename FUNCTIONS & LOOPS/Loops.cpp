60be9397bbc27fcba6fdc803c6166519d8b6346c
userdefined111 <kapilsoni1883@gmail.com>
2026-05-14 23:35:25 +0530

    14/05/2026

--- FUNCTIONS & LOOPS/Loops.cpp
+++ FUNCTIONS & LOOPS/Loops.cpp
@@ -1,214 +1,269 @@
-//program to calculate the sum of first n natural numbers using a loop
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int n;
-//     cout<<"enter the number: ";
-//     cin>>n;
-//     int sum=0;
-//     for(int i=0;i<=n;i++){
-//         sum=i*(i+1)/2;
-//     }
-//     cout<<"the sum is: "<<sum<<endl;
-//     return 0;
-// }
+#include <iostream>
+using namespace std;
 
+// 1. Sum of first n natural numbers
+void sumOfNaturalNumbers() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int i=10;
-//     while(i<=25)
-// {
-//     cout<<i<<" ";
-//     i++;
-// }    
-// return 0;
-// }
+    cout << "Enter the number: ";
+    cin >> n;
 
+    int sum = 0;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-// int n;
-// cout << "enter the number of lines: ";
-// cin >> n;
-//     for(int i=0;i<=n;i++){
-//         for(int j=0;j<=i;j++){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// return 0;
-// }
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
 
+    cout << "The sum is: " << sum << endl;
+}
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "enter the number: ";
-//     cin >> n;
-//     int digitSum=0;
-//     while( n>0 ){
-//         int lastdigit=n%10;
-//         if( lastdigit % 2 != 0 ){
-//             digitSum+=lastdigit;
-//         }
-//         n/=10;
-//     }
-//     cout << "the sum is: " << digitSum << endl;
-//     return 0;
-// }
+// 2. Print numbers from 10 to 25
+void printNumbers10To25() {
+    int i = 10;
 
+    while (i <= 25) {
+        cout << i << " ";
+        i++;
+    }
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "enter the number: ";
-//     cin >> n;
-//     int factorial=1;
-//     for(int i=1;i<=n;i++){
-//         factorial*=i;
-//     }
-//     cout << "the factorial is: " << factorial << endl;
-//     return 0;
-// }
+    cout << endl;
+}
 
+// 3. Print star pattern
+void starPattern() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "enter the number to reverse: ";
-//     cin >> n;
-//     while(n>0){
-//         int lastdigit=n%10;
-//         cout<< lastdigit;
-//         n/=10;
-//     }
-//     cout<<"\n";
-//     return 0;
-// }
+    cout << "Enter the number of lines: ";
+    cin >> n;
 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "enter the number to reverse: ";
-//     cin >> n;
-//     int res=0;
-//     while(n>0){
-//         int lastdigit=n%10;
-//         res=res*10+lastdigit;
-//         n/=10;
-//     }
-//     cout<<res<<"\n";
-//     return 0;
-// }
+        cout << endl;
+    }
+}
 
+// 4. Sum of odd digits of a number
+void sumOfOddDigits() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//    int n=1;
-//     do{
-//        cout<<"hello world in do-while loop\n";
-//    }
-//    while(n>5){
-//        cout<<"hello world in while loop\n";
-//    }
-//    return 0;
-// }
+    cout << "Enter the number: ";
+    cin >> n;
 
+    int digitSum = 0;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     while(1>0)
-//     {   cout<<"enter the number: ";
-//         cin>>n;
-//         if(n%10==0){
-//             continue;
-//         }
-//         cout<<"the number is: "<<n<<endl;
-//     }
-//     return 0;
-// }
+    while (n > 0) {
+        int lastDigit = n % 10;
 
+        if (lastDigit % 2 != 0) {
+            digitSum = digitSum + lastDigit;
+        }
 
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int n;
-//     cout << "enter a number: ";
-//     cin >> n;
-//     bool isPrime = true;   
-//     if (n <= 1) {
-//         isPrime = false;
-//     }   
-//     for (int i = 2; i * i <= n; i++) {
-//         if (n % i == 0) {
-//             isPrime = false;
-//             break;
-//         }
-//     }   
-//     if (isPrime) {
-//         cout << n << " is a prime number" << endl;
-//     } else {
-//         cout << n << " is not a prime number" << endl;
-//     }
-//     return 0;
-// }
+        n = n / 10;
+    }
 
+    cout << "The sum of odd digits is: " << digitSum << endl;
+}
 
-// #include <iostream>
-// using namespace std;    
-// int main() {
-//     int n;
-//     cout << "enter a number: ";
-//     cin >> n;
-//     int fact=1;
-//     for(int i=1;i<=n;i++)
-//     {
-//         fact*=i;
-//     }
-//     cout<<"the factorial is: "<<fact<<endl;
-//     return 0;
-// }
+// 5. Factorial of a number
+void factorialOfNumber() {
+    int n;
 
+    cout << "Enter the number: ";
+    cin >> n;
 
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int n;
-//     cout << "enter a number: ";
-//     cin >> n;
-//     for(int i=1;i<=10;i++)
-//  {
-//    cout<<n*i<<endl;
-//  }
-// return 0;
-// }
+    int factorial = 1;
 
+    for (int i = 1; i <= n; i++) {
+        factorial = factorial * i;
+    }
 
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int n;
-//     cout << "enter a number: ";
-//     cin >> n;
-//     int first=0,sec=1;
-//     cout<<first<<" "<<sec<<" ";
-// for(int i=2;i<n;i++)
-// {
-//     int third=first+sec;
-//     cout<<third<<" ";
-//     first=sec;
-//     sec=third;
-// }
-// cout<<"\n";
-// }
+    cout << "The factorial is: " << factorial << endl;
+}
+
+// 6. Reverse number by printing digits
+void reverseNumberByPrinting() {
+    int n;
+
+    cout << "Enter the number to reverse: ";
+    cin >> n;
+
+    cout << "Reversed number is: ";
+
+    while (n > 0) {
+        int lastDigit = n % 10;
+        cout << lastDigit;
+        n = n / 10;
+    }
+
+    cout << endl;
+}
+
+// 7. Reverse number and store result
+void reverseNumberStored() {
+    int n;
+
+    cout << "Enter the number to reverse: ";
+    cin >> n;
+
+    int result = 0;
+
+    while (n > 0) {
+        int lastDigit = n % 10;
+        result = result * 10 + lastDigit;
+        n = n / 10;
+    }
+
+    cout << "Reversed number is: " << result << endl;
+}
+
+// 8. Do while loop example
+void doWhileExample() {
+    int n = 1;
+
+    do {
+        cout << "Hello world in do-while loop" << endl;
+    } while (n > 5);
+}
+
+// 9. Continue example, skip numbers ending with 0
+void continueExample() {
+    int n;
+
+    while (true) {
+        cout << "Enter the number: ";
+        cin >> n;
+
+        if (n % 10 == 0) {
+            continue;
+        }
+
+        cout << "The number is: " << n << endl;
+    }
+}
+
+// 10. Check prime number
+void checkPrimeNumber() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    bool isPrime = true;
+
+    if (n <= 1) {
+        isPrime = false;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        cout << n << " is a prime number" << endl;
+    } else {
+        cout << n << " is not a prime number" << endl;
+    }
+}
+
+// 11. Multiplication table
+void multiplicationTable() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Multiplication table of " << n << " is:" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+}
+
+// 12. Fibonacci series
+void fibonacciSeries() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    int first = 0;
+    int second = 1;
+    if (n <= 0) {
+        cout << "Invalid input" << endl;
+    } else if (n == 1) {
+        cout << first << endl;
+    } else {
+        cout << first << " " << second << " ";
+        for (int i = 2; i < n; i++) {
+            int third = first + second;
+            cout << third << " ";
+            first = second;
+            second = third;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int choice;
+
+    cout << "Choose an operation:" << endl;
+    cout << "1. Sum of first n natural numbers" << endl;
+    cout << "2. Print numbers from 10 to 25" << endl;
+    cout << "3. Star pattern" << endl;
+    cout << "4. Sum of odd digits" << endl;
+    cout << "5. Factorial" << endl;
+    cout << "6. Reverse number by printing digits" << endl;
+    cout << "7. Reverse number and store result" << endl;
+    cout << "8. Do-while loop example" << endl;
+    cout << "9. Continue example" << endl;
+    cout << "10. Check prime number" << endl;
+    cout << "11. Multiplication table" << endl;
+    cout << "12. Fibonacci series" << endl;
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            sumOfNaturalNumbers();
+            break;
+        case 2:
+            printNumbers10To25();
+            break;
+        case 3:
+            starPattern();
+            break;
+        case 4:
+            sumOfOddDigits();
+            break;
+        case 5:
+            factorialOfNumber();
+            break;
+        case 6:
+            reverseNumberByPrinting();
+            break;
+        case 7:
+            reverseNumberStored();
+            break;
+        case 8:
+            doWhileExample();
+            break;
+        case 9:
+            continueExample();
+            break;
+        case 10:
+            checkPrimeNumber();
+            break;
+        case 11:
+            multiplicationTable();
+            break;
+        case 12:
+            fibonacciSeries();
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+    }
+    return 0;
+}
--- PATTERNS/Patterns.cpp
+++ PATTERNS/Patterns.cpp
@@ -1,217 +1,299 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "Enter the number of rows: ";
-//     cin >> n;
-//     for (int i=1;i<=n;i++){
-//         for(int j=1;j<=n;j++){
-//             cout<<i<<"*4 ";
-//         }
-//         cout<<endl;
-//     }
-//     return 0;
-// }
+#include <iostream>
+using namespace std;
 
+// 1. Print i*4 pattern
+void patternRowMultiply4() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "Enter the number of rows: ";
-//     cin >> n;
-//     for(int i =1;i<=n;i++){
-//         for ( int j =n;j>=i;j--){
-//             cout<<"* ";
-//         }
-//         cout<<endl;
-//     }
-//     return 0;
-// }
+    cout << "Enter the number of rows: ";
+    cin >> n;
 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << i << "*4 ";
+        }
+        cout << endl;
+    }
+}
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "Enter the number of rows: ";
-//     cin >> n;
-//     for (int i=1;i<=n;i++){
-//         for (int j=1;j<=i;j++){
-//             cout<<j<<" ";
-//         }
-//         cout<<endl;
-//     }
-//     return 0;
-// }
+// 2. Inverted star triangle
+void invertedStarTriangle() {
+    int n;
 
+    cout << "Enter the number of rows: ";
+    cin >> n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout << "Enter the number of rows: ";
-//     cin >> n;
-//     for (int i=1;i<=n;i++){
-//         for (int j=1;j<=i;j++){
-//             cout<<i<<" ";
-//         }
-//         cout<<endl;
-//     }
-//     return 0;
-// }
+    for (int i = 1; i <= n; i++) {
+        for (int j = n; j >= i; j--) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 
+// 3. Number triangle: 1 to i
+void numberTriangleIncreasingColumn() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter the number of rows: ";
-//     cin>>n;
-//     char ch='A';
-//     for (int i=1;i<=n;i++){
-//         for (int j=1;j<=i;j++){
-//             cout<<ch++<<" ";
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
+    cout << "Enter the number of rows: ";
+    cin >> n;
 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter the number of rows: ";
-//     cin>>n;
-//     char ch='A';
-//     for (int i=1;i<=n;i++){
-//         for (int j=1;j<=i;j++){
-//             cout<<ch<<" ";
-//         }
-//         ch++;
-//         cout << endl;
-//     }
-//     return 0;
-// }
+// 4. Number triangle: same row number
+void numberTriangleSameRow() {
+    int n;
 
+    cout << "Enter the number of rows: ";
+    cin >> n;
 
-// #include <iostream>
-// using namespace std;        
-// int main(){
-//     int n;
-//     cout<<"enter the number of rows: ";
-//     cin>>n;
-//     for (int i=1;i<=n;i++){
-//         cout<<"*"<<" ";
-//         for(int j=1;j<=n-1;j++){        
-//         if(i==1 || i==n){
-//             cout<<"*"<<" ";
-//         }
-//         else{
-//             cout<<" "<<" ";
-//         }
-//     }
-//         cout<<"*"<<endl;
-//     }
-//     return 0;
-// }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
 
+// 5. Continuous alphabet triangle
+void continuousAlphabetTriangle() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter the number of rows: ";
-//     cin>>n;    
-//     for (int i=1;i<=n;i++){
-//        for(int j=1;j<=n-i;j++){
-//         cout<<" ";
-//        } 
-//        for (int J=1;J<=i;J++){
-//         cout<<"* ";
-//        }
-//          cout<<endl;
-//     }
-//     return 0;
-// }
+    cout << "Enter the number of rows: ";
+    cin >> n;
 
+    char ch = 'A';
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter the number of rows: ";
-//     cin>>n;
-//     int a=1;
-//     for (int i=1;i<=n;i++){       
-//         for (int j=1;j<=i;j++){
-//             cout<<a++<<" ";   
-//         }
-//         cout<<endl;
-//     } 
-//     return 0;
-// }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << ch++ << " ";
+        }
+        cout << endl;
+    }
+}
 
+// 6. Same alphabet in each row
+void sameAlphabetRowTriangle() {
+    int n;
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter the number of rows: ";
-//     cin>>n;
-//     for(int i=1;i<=n; i++){
-//         for (int j=1;j<=n-i;j++){
-//             cout<<" ";
-//         }
-//         for(int j=1;j<=2*i-1;j++){
-//             cout<<"*";
-//         }        
-//         cout<<endl;
-//     }
-//     for (int i=n;i>=1;i--){
-//         for (int j=1;j<=n-i;j++){
-//             cout<<" ";
-//         }
-//         for(int j=1;j<=2*i-1;j++){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-//     return 0;
-// }
+    cout << "Enter the number of rows: ";
+    cin >> n;
 
+    char ch = 'A';
 
-// #include <iostream>
-// using namespace std;
-// int main(){
-// int n;
-// cout<<"enter the number of rows: ";
-// cin>>n;
-// for (int i=1;i<=n;i++){
-//     for (int j=1;j<=i;j++){
-//         cout<<"*"<<" ";
-//     }
-//     for(int j=1;j<2*(n-i);j++){
-//         cout<<" ";
-//     }
-//     for (int j=1;j<=i;j++){
-//         cout<<"*"<<" ";
-//     }
-//     cout<<endl;
-// }
-// for (int i=n;i>=1;i--){
-//     for (int j=1;j<=i;j++){
-//         cout<<"*"<<" ";
-//     }
-//     for(int j=1;j<=2*(n-i);j++){
-//         cout<<" ";
-//     }
-//     for( int j=1;j<=i;j++){
-//         cout<<"*"<<" ";
-//     }
-//     cout<<endl;
-// }
-// return 0;
-// }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << ch << " ";
+        }
+        ch++;
+        cout << endl;
+    }
+}
+
+// 7. Hollow rectangle
+void hollowRectangle() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "* ";
+
+        for (int j = 1; j <= n - 1; j++) {
+            if (i == 1 || i == n) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+
+        cout << "*" << endl;
+    }
+}
+
+// 8. Right aligned star triangle
+void rightAlignedStarTriangle() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+// 9. Floyd's triangle
+void floydTriangle() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    int a = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << a++ << " ";
+        }
+        cout << endl;
+    }
+}
+
+// 10. Diamond pattern
+void diamondPattern() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+// 11. Butterfly pattern
+void butterflyPattern() {
+    int n;
+
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        for (int j = 1; j < 2 * (n - i); j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    int choice;
+
+    cout << "Choose a pattern:" << endl;
+    cout << "1. Row number multiplied by 4 pattern" << endl;
+    cout << "2. Inverted star triangle" << endl;
+    cout << "3. Number triangle 1 to i" << endl;
+    cout << "4. Same row number triangle" << endl;
+    cout << "5. Continuous alphabet triangle" << endl;
+    cout << "6. Same alphabet row triangle" << endl;
+    cout << "7. Hollow rectangle" << endl;
+    cout << "8. Right aligned star triangle" << endl;
+    cout << "9. Floyd's triangle" << endl;
+    cout << "10. Diamond pattern" << endl;
+    cout << "11. Butterfly pattern" << endl;
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            patternRowMultiply4();
+            break;
+
+        case 2:
+            invertedStarTriangle();
+            break;
+
+        case 3:
+            numberTriangleIncreasingColumn();
+            break;
+
+        case 4:
+            numberTriangleSameRow();
+            break;
+
+        case 5:
+            continuousAlphabetTriangle();
+            break;
+
+        case 6:
+            sameAlphabetRowTriangle();
+            break;
+
+        case 7:
+            hollowRectangle();
+            break;
+
+        case 8:
+            rightAlignedStarTriangle();
+            break;
+
+        case 9:
+            floydTriangle();
+            break;
+
+        case 10:
+            diamondPattern();
+            break;
+
+        case 11:
+            butterflyPattern();
+            break;
+
+        default:
+            cout << "Invalid choice" << endl;
+    }
+
+    return 0;
+}
