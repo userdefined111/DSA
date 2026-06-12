049c9ef6f245a1d38a7a1f3bd9342e7e36204183
kapil soni <kapilsoni1883@gmail.com>
2026-06-13 02:10:24 +0530

    13/06/2026

--- STACK/Stack1.cpp
+++ STACK/Stack1.cpp
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// class Stack{
-//     vector<int>vec;
-// public:
-//     void push(int val){
-//         vec.push_back(val);
-//     }
-//     void pop(){
-//         if(isEmpty()){
-//             cout<<"stack is empty.\n";
-//             return;
-//         }
-//         vec.pop_back();
-//     }
-//     int top(){
-//         int lastIdx= vec.size()-1;
-//         return vec[lastIdx];
-//     }
-//     bool isEmpty(){
-//         return vec.size()==0;
-//     }
-// };
-
-template<class T>
+// create stack using vector
 class Stack{
+    vector<int>vec;
+public:
+    void push(int val){
+        vec.push_back(val);
+    }
+    void pop(){
+        if(isEmpty()){
+            cout<<"stack is empty.\n";
+            return;
+        }
+        vec.pop_back();
+    }
+    int top(){
+        int lastIdx= vec.size()-1;
+        return vec[lastIdx];
+    }
+    bool isEmpty(){
+        return vec.size()==0;
+    }
+};
+
+// create stack using vector with template 
+template<class T>
+class NStack{
     vector<T>vec;
 public:
     void push(T val){
@@ -46,26 +48,26 @@ public:
     }
 };
 
-// int main(){
-//     Stack s;
-//     s.push(3);
-//     s.push(2);
-//     s.push(1);
-//     while(!s.isEmpty()){
-//         cout<<s.top()<<" ";
-//         s.pop();
-//     }
-//     return 0;
-// }
-
 int main(){
-    Stack<char> s;
-    s.push('c');
-    s.push('b');
-    s.push('a');
-    while(!s.isEmpty()){
-        cout<<s.top()<<" ";
-        s.pop();
+    NStack<char> s1;
+
+    s1.push('c');
+    s1.push('b');
+    s1.push('a');
+    while(!s1.isEmpty()){
+        cout<<s1.top()<<" ";
+        s1.pop();
+    }
+    cout<<"\n";
+
+    Stack s2;
+
+    s2.push(3);
+    s2.push(2);
+    s2.push(1);
+    while(!s2.isEmpty()){
+        cout<<s2.top()<<" ";
+        s2.pop();
     }
     return 0;
 }
