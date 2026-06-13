f99d82284e7965713ddf14ab247de2b8bb92cc48
kapil soni <kapilsoni1883@gmail.com>
2026-06-13 21:54:42 +0530

    13/06/2026

--- STACK/Stack1.cpp
+++ STACK/Stack1.cpp
@@ -48,7 +48,44 @@ public:
     }
 };
 
+void pushatbottom(stack<int>& s4, int val){
+    if(s4.empty()){
+        s4.push(val);
+        return;
+    }
+    int temp =s4.top();
+    s4.pop();
+    pushatbottom(s4, val);
+    s4.push(temp);
+}
+
+void ReverseString(string str){
+    string ans;
+    stack<char> s5;
+    
+    for(int i=0; i<str.size(); i++){
+        s5.push(str[i]);
+    }
+    while(!s5.empty()){
+        char top=s5.top();
+        ans+=top;
+        s5.pop();
+    }
+    cout<<ans<<"\n";
+}
+
+void ReverseStack(stack<int>& s6){
+    stack<int> temp;
+    while(!s6.empty()){
+        int top=s6.top();
+        temp.push(top);
+        s6.pop();
+    }
+    s6=temp; 
+}
+
 int main(){
+    cout<<"stack of template class: \n";
     NStack<char> s1;
 
     s1.push('c');
@@ -60,6 +97,7 @@ int main(){
     }
     cout<<"\n";
 
+    cout<<"stack of class: \n";
     Stack s2;
 
     s2.push(3);
@@ -69,5 +107,46 @@ int main(){
         cout<<s2.top()<<" ";
         s2.pop();
     }
+    cout<<"\n";
+
+    cout<<"stack of STL: \n";
+    stack<int> s3;
+    s3.push(3); 
+    s3.push(2);
+    s3.push(1);
+    while(!s3.empty()){
+        cout<<s3.top()<<" ";
+        s3.pop();
+    }
+    cout<<"\n";
+
+    cout<<"push at bottom of stack: \n";
+    stack<int> s4;
+    s4.push(1);
+    s4.push(2);
+    s4.push(3);
+    pushatbottom(s4, 11);
+    while(!s4.empty()){
+        cout<<s4.top()<<" ";
+        s4.pop();
+    }
+    cout<<"\n";
+
+    cout<<"reverse string using stack: \n";
+    string str="Ambulance";
+    ReverseString(str);
+
+    cout<<"reverse stack using stack: \n";
+    stack<int> s6;
+    s6.push(1);
+    s6.push(2);
+    s6.push(3);
+    ReverseStack(s6);
+    while(!s6.empty()){
+        cout<<s6.top()<<" ";
+        s6.pop();
+    }
+    cout<<"\n";
+
     return 0;
 }
