ebf49225da45b33087a1764cbd8208504af0fb58
kapil soni <kapilsoni1883@gmail.com>
2026-06-26 15:56:59 +0530

    26/06/2026

--- STACK/Stack1.cpp
+++ STACK/Stack1.cpp
@@ -123,17 +123,65 @@ bool duplicateparanthesis(string str){
             s.push(ch);
         }
         else{
-            if( s.top()=='('){
-                return true "\n"; // duplicate paranthesis
-            }
-            while(s.top()!='('){
+            if( s.top()=='('){return true ; }// duplicate paranthesis
+            if(s.top()=='('){return true ; }// duplicate paranthesis
+                while(s.top()!='('){
                 s.pop();
             }
-            s.pop();
+            if(!s.empty()){
+                s.pop();
+            }
         }
     }
-    return false "\n";
+    return false ; // no duplicate paranthesis
 } 
+
+int MaxAreaHistogram(vector<int> height){
+    int n=height.size();
+    vector<int>nsl(n), nsr(n);
+    stack<int> s;
+
+    //next smaller left
+    nsl[0]=-1;
+    s.push(0);
+    for(int i=1;i<height.size();i++){
+        int curr=height[i]; 
+        while(!s.empty() && curr<=height[s.top()]){
+            s.pop();
+        }
+        if(s.empty()){
+            nsl[i]=-1;
+        }
+        else{
+            nsl[i]=s.top();
+        }
+        s.push(i);
+    }
+    while(!s.empty()){
+        s.pop();
+    }
+
+    //next smaller right
+    int n=height.size();
+    s.push(n-1);
+    nsr[n-1]=n;
+    for(int i=n-2;i>=0;i--){
+        int curr=height[i];
+        while(!s.empty()&& curr<=height[s.top()]){
+            s.pop();
+        }
+        nsr[i]=s.empty()?n:s.top();
+        s.push(i);
+    }
+
+    int maxArea=0;
+    for(int i=0;i<n;i++){
+        int width=nsr[i]-nsl[i]-1;
+        int area=width*height[i];
+        maxArea=max(maxArea,area);
+    }
+    return maxArea;
+}
 
 int main(){     
     cout<<"stack of template class: \n";
@@ -221,5 +269,7 @@ int main(){
         cout<<"not duplicate paranthesis.\n";
     }
 
+    cout<<"Maximum area in histogram: "<<MaxAreaHistogram({2,1,5,6,2,3})<<endl;
+    
     return 0;
 }
