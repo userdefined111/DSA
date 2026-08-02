b02d532697b5fa9f891946dd39ae6ea320cd84b1
kapil soni <kapilsoni1883@gmail.com>
2026-08-02 21:57:05 +0530

    02/08/2026

---
+++ Queue/StackUsingQueue.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int data){
+        q1.push(data);
+    }   
+
+    void pop(){
+
+    }
+
+}
