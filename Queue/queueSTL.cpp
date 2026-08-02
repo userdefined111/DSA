4e9999fdb52f26903204f5ee843030f0bb702489
kapil soni <kapilsoni1883@gmail.com>
2026-08-02 21:57:05 +0530

    03/08/2026

---
+++ Queue/queueSTL.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    while (!q.empty())
+    {
+        cout << q.front() << "\n";
+        q.pop();
+    }
+}
